9efa1cdb6ae774660b4613ad50dc3708589cc8dd
dandersonstack <danieltanderson@berkeley.edu>
2017-05-08 21:49:36 -0700

    added uniq and reject tests

--- test/underbar/reject.test.js
+++ test/underbar/reject.test.js
@@ -2,10 +2,14 @@ const _ = require('../../underbar');
 
 describe('reject()', () => {
   it('rejects odd numbers from an array', () => {
-    // Your code goes here
+    const arr = [1, 3, 6, 8];
+    const val = _.reject(arr, item => item % 2 == 1);
+    expect(val).toEqual([6, 8]);
   });
 
   it('rejects null values from an object', () => {
-    // Your code goes here
+    const arr = [1, 3, null, null];
+    const val = _.reject(arr, item => item === null);
+    expect(val).toEqual([1, 3]);
   });
 });
--- test/underbar/uniq.test.js
+++ test/underbar/uniq.test.js
@@ -2,6 +2,8 @@ const _ = require('../../underbar');
 
 describe('uniq()', () => {
   it('de-dups a list of numbers', () => {
-    // Your code goes here
+    const arr = [1, 3, 3, 4, 4];
+    const val = _.uniq(arr);
+    expect(val).toEqual([1, 3, 4]);
   });
 });
