8c59290db1d9144b9b863adc5a4ba167bf204368
dandersonstack <danieltanderson@berkeley.edu>
2017-05-08 21:47:05 -0700

    added first, last, and identity tests

--- test/underbar/indexOf.test.js
+++ test/underbar/indexOf.test.js
@@ -2,23 +2,28 @@ const _ = require('../../underbar');
 
 describe('indexOf()', () => {
   it('returns the index of a value at the beginning of an array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4];
+    expect(_.indexOf(arr, 1)).toEqual(0);
   });
 
   it('returns the index of a value at the end of an array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4];
+    expect(_.indexOf(arr, 4)).toEqual(arr.length - 1);
   });
 
   it('returns -1 for a missing value', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4];
+    expect(_.indexOf(arr, 7)).toEqual(-1);
   });
 
   it('returns the first matching index when multiple matches in array', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 2, 3, 4];
+    expect(_.indexOf(arr, 3)).toEqual(2);
   });
 
   it('starts searching at the given offset', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 2, 3, 4];
+    expect(_.indexOf(arr, 3, 4)).toEqual(5);
   });
 
 });
