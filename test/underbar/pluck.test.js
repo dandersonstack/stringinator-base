42f2505406904cb1a8ce1c5b5608ca034d57b0f6
dandersonstack <danieltanderson@berkeley.edu>
2017-05-08 21:48:30 -0700

    added pluck and reduce tests

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -2,7 +2,10 @@ const _ = require('../../underbar');
 
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
-    // Your code goes here
+    const arr = [{name: 'daniel', age: 14}, 
+                 {name: 'ron', age: 11}, 
+                 {name: 'quill', age: 2} ];
+    expect(_.pluck(arr, 'age')).toEqual([14, 11, 2]);
   });
 
 });
--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,15 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const arr = [1,2,3,4,5];
+    const val = _.reduce(arr, (sum, item) => sum + item , 10);
+    expect(val).toEqual(25);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    const arr = [1,2,3,4,5];
+    const val = _.reduce(arr, (sum, item) => sum + item , 0);
+    expect(val).toEqual(15);
   });
 
 });
