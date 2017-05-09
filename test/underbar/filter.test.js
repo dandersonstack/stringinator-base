bc08c73470750cb5d8e9940b87403a521f4162ee
dandersonstack <danieltanderson@berkeley.edu>
2017-05-08 21:49:13 -0700

    added filter tests

--- test/underbar/filter.test.js
+++ test/underbar/filter.test.js
@@ -2,10 +2,14 @@ const _ = require('../../underbar');
 
 describe('filter()', () => {
   it('filters an array to odd numbers', () => {
-    // Your code goes here
+    const arr = [1, 3, 6, 8];
+    const val = _.filter(arr, item => item % 2 == 1);
+    expect(val).toEqual([1, 3]);
   });
 
   it('filters an object to only numeric values', () => {
-    // Your code goes here
+    const arr = [1, 3, 6, 8, 'book'];
+    const val = _.filter(arr, item => typeof(item) == 'number');
+    expect(val).toEqual([1, 3, 6, 8]);
   });
 });
